20a92995f5b8154371315f894f2d5162c31746be
Riley Kohler <riley.kohler@capitalone.com>
2023-04-04 15:28:53 -0400

    test

---
+++ test.py
@@ -0,0 +1,8 @@
+## Contributing
+
+1. Fork it (<https://github.com/finos/morphir-scala/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Read our [contribution guidelines](.github/CONTRIBUTING.md) and [Community Code of Conduct](https://www.finos.org/code-of-conduct)
+4. Commit your changes (`git commit -am 'Add some fooBar'`)
+
+_NOTE:_ Commits and pull requests to FINOS repositories will only be accepted from those contributors with an active, executed Individual Contributor License Agreement (ICLA) with FINOS OR who are covered under an existing and active Corporate Contribution License Agreement (CCLA) executed with FINOS.
